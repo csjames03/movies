af9b27c5fa5b45cfbd1a6e833d9d04090f370c7e
csjames <csjames0303@gmail.com>
2023-01-27 11:26:12 +0800

    added a copyright

--- app/layout.jsx
+++ app/layout.jsx
@@ -10,14 +10,20 @@ export default function RootLayout({ children }) {
       */}
       <head />
       <body>
-        <nav className='w-full h-auto flex justify-evenly p-5 fixed bg-neutral-900'>
+        <nav className='w-full h-auto flex justify-evenly p-5 fixed bg-neutral-900 z-10'>
           <Link href={"/"} className="hover:text-red-500 hover:scale-110 transition duration-300 ease-in-out">Popular</Link>
-          <Link href={"/"} className="hover:text-red-500 hover:scale-110 transition duration-300 ease-in-out">Latest</Link>
+          <Link href={"/latest"} className="hover:text-red-500 hover:scale-110 transition duration-300 ease-in-out">Latest</Link>
           <Link href={"/toprated"} className="hover:text-red-500 hover:scale-110 transition duration-300 ease-in-out">Top Rated</Link>
-          <Link href={"/"} className="hover:text-red-500 hover:scale-110 transition duration-300 ease-in-out">Upcoming</Link>
+          <Link href={"/upcoming"} className="hover:text-red-500 hover:scale-110 transition duration-300 ease-in-out">Upcoming</Link>
         </nav>
         {children}
+        <div className='w-screen h-auto p-2 flex justify-center'>
+         &copy; 
+         
+         <Link href={"https://github.com/csjames03/"}>James Ocao 2023</Link>
+        </div>
         </body>
+        
     </html>
   )
 }
